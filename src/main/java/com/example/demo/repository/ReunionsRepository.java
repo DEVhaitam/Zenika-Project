a7a51f3fe81b3582b7e617d467ab181f42e9e042
haitam <haitamelhayanitech@gmail.com>
2023-11-08 03:10:34 +0100

    created repositories for crud operations

--- src/main/java/com/example/demo/repository/ReunionsRepository.java
+++ src/main/java/com/example/demo/repository/ReunionsRepository.java
@@ -1,7 +1,9 @@
 package com.example.demo.repository;
 
+import com.example.demo.model.Reunions;
 import com.example.demo.model.Salles;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface SalleRepository extends JpaRepository<Salles,String> {
+public interface ReunionsRepository extends JpaRepository<Reunions,Integer> {
+
 }
